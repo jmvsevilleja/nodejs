59dd072180b1242085ed00f82e12c0b8d7cfbc9c
Jess Mark Sevilleja <jsevilleja@bti00010951.ph.esl-asia.com>
2018-10-25 09:59:46 +0800

    class events

---
+++ log.js
@@ -0,0 +1,9 @@
+
+function log(message) {
+	// Send an HTTP request
+	console.log(message);
+	console.log(__dirname);
+	console.log(__filename);
+
+}
+module.exports = log;
--- logger.js
+++ logger.js
@@ -1,12 +1,25 @@
+
+const EventEmitter = require('events');
+
 var url = 'http://mylogger.io/log';
 
+class Logger extends EventEmitter{
+	log(message) {
+		// Send an HTTP request
+		console.log(message);
 
-function log(message) {
+		// Raise an event
+		this.emit('messageLogged', { id: 1, url: 'http://'});
+	}
+}
+
+/* function log(message) {
 	// Send an HTTP request
 	console.log(message);
 	console.log(__dirname);
 	console.log(__filename);
 
 }
-
 module.exports = log;
+ */
+module.exports = Logger;
